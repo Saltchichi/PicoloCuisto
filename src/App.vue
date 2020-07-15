883354a6c8e89d4274891f8ee479adf4018cf9ab
Alexandre MONSCHEIN <a.monschein@lyon.ort.asso.fr>
2020-07-15 15:04:09 +0200

    adding logo and modifying hello world

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo-no-bg.png" />
+    <Accueil msg="Welcome to PicoloCuisto's Website" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Accueil from "./components/Accueil.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Accueil,
+  },
+};
 </script>
 
 <style>
---
+++ src/components/Accueil.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="accueil">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Accueil",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<style scoped>
+</style>
