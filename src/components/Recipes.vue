ed96e8ce0e2255706bf70cf04751a5308c827f2f
AGoubs <arnaud@goubier.fr>
2020-07-22 17:24:28 +0200

    Adapting recipes to viewRecipe

--- src/components/Recipes.vue
+++ src/components/Recipes.vue
@@ -3,22 +3,20 @@
     <!-- <div class="row mx-4"> -->
     <div class="card-columns mx-4">
       <div v-for="recipes in allRecipes" :key="recipes" class="card mr-4 mt-4">
-        <img
-          class="card-img-top"
-          v-bind:src="getImgUrl(recipes.chemin_image)"
-          alt="Image recette"
-        />
+        <img class="card-img-top" v-bind:src="getImgUrl(recipes.chemin_image)" alt="Image recette" />
         <!-- <img class="card-img-top" src="../assets/tarte_aux_framboises.jpg" alt="Card image cap" /> -->
         <div class="card-body">
           <h5 class="card-title">{{ recipes.nom_fr }}</h5>
           <p class="card-text">{{ recipes.presentation_fr }}</p>
-          <p class="card-text"><i class="far fa-clock"></i>{{ recipes.presentation_fr }}Heures</p>
+          <p class="card-text">
+            <i class="far fa-clock"></i> {{ displayHour(recipes.heure) }} {{ displayMinute(recipes.minute) }}
+          </p>
           <p class="card-text">
             <small class="text-muted">Pour {{ recipes.cb_personne }} personnes</small>
           </p>
         </div>
         <div class="card-footer">
-          <a href="#" class="btn btn-outline-danger">Consulter la recette</a>
+          <button type="submit" class="btn btn-outline-danger" v-on:click="consult(recipes)">Consulter la recette</button>
         </div>
       </div>
     </div>
@@ -48,6 +46,21 @@ export default {
       });
   },
   methods: {
+    displayHour(hour) {
+      if (hour == null) {
+        return "";
+      } else {
+        return hour + " h";
+      }
+    },
+    displayMinute(min) {
+      if (min == null) {
+        return "";
+      }
+      else {
+        return min + ' min';
+      }
+    },
     consult(recipe) {
       localStorage.setItem("id_recette_to_consult", recipe.id_recette);
       router.push({ name: "viewRecipe" });
@@ -60,7 +73,7 @@ export default {
       }
     }
   }
-}
+};
 </script>
 
 <style>
